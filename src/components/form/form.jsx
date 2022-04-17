217d59e89e2cafb9cf72a367d75a83f8dc62cc2f
cristianUrbina <A01412249@itesm.mx>
2022-04-17 17:33:04 -0500

    Add submit button functionality

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -1,17 +1,38 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
 import "./form.css";
 
+import Popup from "../popup/popup.jsx";
+
 export default function Form() {
+  useEffect(() => {
+    const myButton = document.getElementById("myButton");
+
+    myButton.addEventListener("click", (e) => {
+      e.preventDefault();
+    });
+  }, []);
+
+  const [state, setState] = useState({
+    active: false,
+  });
+
+  const toggleModal = () => {
+    setState((state) => ({
+      active: !state.active,
+    }));
+  };
+
   return (
     <div className="form">
       <h1>Calculo de esfuerzo</h1>
-      <form className="main-form" action="" method="POST">
+      <form className="main-form" action="">
         <label for="summary">Resumen de historia de usuario</label>
         <input id="summary" type="text" name="summary" />
         <label for="description">Descripcion de historia de usuario</label>
         <input id="description" type="text" name="description" />
-        <input type="submit" value="Calcular esfuerzo" />
+        <input id="myButton" type="submit" value="Calcular esfuerzo" onClick={toggleModal} />
       </form>
+      {state.active ? <Popup /> : ""}
     </div>
   );
 }
---
+++ src/components/popup/popup.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./popup.css";
+
+export default function Popup() {
+  return (
+    <div className="popup">
+      Hello
+    </div>
+  );
+}
