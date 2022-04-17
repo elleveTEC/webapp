1e1337466f6374976517c3a4a60986869824ae75
cristianUrbina <A01412249@itesm.mx>
2022-04-17 16:47:30 -0500

    Add top bar

---
+++ src/components/header/header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./header.css";
+
+export default function TopBar () {
+  return (
+    <div className="top-bar">
+      <img className="logo" src="/images/CEMEXlogo.png" />
+    </div>
+  );
+}
