b3dd56faf823e969ba9229578d137f816aec86c0
cristianUrbina <A01412249@itesm.mx>
2022-04-25 19:22:35 -0500

    Add logic to turn the submit button blue

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -14,22 +14,41 @@ export default function Form() {
 
   const [state, setState] = useState({
     active: false,
+    summaryLength: 0,
+    descriptionLength: 0,
   });
 
   const toggleModal = () => {
     setState((state) => ({
-      active: !state.active,
+      ...state, active: !state.active, 
     }));
   };
+
+  const handleChange = (e) => {
+    const content = e.target.value;
+    const value = `${e.target.name}Length`; 
+
+    setState({
+      ...state, [value]: content.length,
+    });
+
+    const btn = document.getElementById("myButton");
+    if (state.summaryLength >= 10 && state.descriptionLength >= 10) {
+      btn.style.backgroundColor = "#3FA9F5";
+    }
+    else {
+      btn.style.backgroundColor = "";
+    }
+  }
 
   return (
     <div className="form">
       <h1>Calculo de esfuerzo</h1>
       <form className="main-form" action="">
-        <label for="summary">Resumen de historia de usuario</label>
-        <input id="summary" type="text" name="summary" />
-        <label for="description">Descripcion de historia de usuario</label>
-        <input id="description" type="text" name="description" />
+        <label htmlFor="summary">Resumen de historia de usuario</label>
+        <input id="summary" type="text" name="summary" onChange={handleChange}/>
+        <label htmlFor="description">Descripcion de historia de usuario</label>
+        <input id="description" type="text" name="description" onChange={handleChange}/>
         <input id="myButton" type="submit" value="Calcular esfuerzo" onClick={toggleModal} />
       </form>
       {state.active ? <Popup correct={true} toggleModal={toggleModal} /> : ""}
