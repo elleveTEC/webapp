0632664c9b73eba92c20ce79b2f528a40577f5f5
cristianUrbina <A01412249@itesm.mx>
2022-04-29 17:07:59 -0500

    Add profile button

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import ProfileButton from "./profileButton/profileButton.jsx";
 import "./header.css";
 
 export default function TopBar() {
   return (
     <div className="header">
       <img className="logo" src="/images/CEMEXlogo.png" />
+      <ProfileButton />
       <div className="red-line"></div>
     </div>
   );
---
+++ src/components/header/profileButton/profileButton.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./profileButton.css";
+
+export default function ProfileButton() {
+  return(
+    <div className="profile-button">
+      {/*<img src="images/profile-icon.png" alt="profile icon" />*/}
+      CU
+    </div>
+  );
+}
