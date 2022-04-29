91b2ecf836bd097c2e0fc644e5730a4da62a33c8
cristianUrbina <A01412249@itesm.mx>
2022-04-29 18:40:36 -0500

    Add profile pop up

--- src/components/header/profileButton/profileButton.jsx
+++ src/components/header/profileButton/profileButton.jsx
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./profileButton.css";
 
 export default function ProfileButton() {
-  return(
-    <div className="profile-button">
-      {/*<img src="images/profile-icon.png" alt="profile icon" />*/}
-      CU
+  const [state, setState] = useState({
+    active: false,
+  });
+  const initials = "CG";
+  return (
+    <div className="profile-button" onClick={() => setState({ active: !state.active })}>
+      {/*<img src="/images/profile-icon.png" alt="profile icon" />*/}
+      <p>{initials}</p>
+      {state.active ? (
+        <div className="pop-up">
+          {/*<img src="/images/profile-icon.png" alt="profile icon" />*/}
+          <p className="initials">{initials}</p>
+          <div className="data">
+            <p className="name">Name Surname</p>
+            <p className="email">user@cemex.com</p>
+          </div>
+          <button>Edit profile</button>
+          <button>Log out</button>
+        </div>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
