512775af0915bee4f195106a6a2241b5ddbda70d
cristianUrbina <A01412249@itesm.mx>
2022-05-03 02:13:18 -0500

    Add name of the task and the Starting date input

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -1,13 +1,12 @@
 import { React, useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
-import  { toggle } from "../../features/opener/openerSlice";
+import { toggle } from "../../features/opener/openerSlice";
 
 import "./form.css";
 
 import Popup from "../popup/popup.jsx";
 
 export default function Form() {
-
   const [state, setState] = useState({
     summaryLength: 0,
     descriptionLength: 0,
@@ -30,8 +29,7 @@ export default function Form() {
     if (state.summaryLength >= 10 && state.descriptionLength >= 10) {
       btn.style.backgroundColor = "#3FA9F5";
       btn.disabled = false;
-    }
-    else {
+    } else {
       btn.style.backgroundColor = "";
       btn.disabled = true;
     }
@@ -43,21 +41,44 @@ export default function Form() {
 
   const handleChange = (e) => {
     const content = e.target.value;
-    const textarea = `${e.target.name}Length`; 
+    const textarea = `${e.target.name}Length`;
 
     setState({
-      ...state, [textarea]: content.length,
+      ...state,
+      [textarea]: content.length,
     });
-  }
+  };
 
   return (
     <div className="form">
-      <h1>Effort Estimation Calculation</h1>
+      <h1>Effor prediction</h1>
       <form className="main-form" action="">
+        <div className="row">
+          <div className="col">
+            <label htmlFor="story-name">Name of the task</label>
+            <input type="text" id="story-name" name="story-name" placeholder="Name of the story" />
+          </div>
+          <div className="col">
+            <label htmlFor="starting-date">Starting date</label>
+            <input type="date" id="starting-date" name="starting-date" />
+          </div>
+        </div>
         <label htmlFor="summary">User story summary</label>
-        <textarea id="summary" type="text" name="summary" placeholder="Summary" onChange={handleChange}/>
+        <textarea
+          id="summary"
+          type="text"
+          name="summary"
+          placeholder="Summary"
+          onChange={handleChange}
+        />
         <label htmlFor="description">User story description</label>
-        <textarea id="description" type="text" name="description" placeholder="Description" onChange={handleChange}/>
+        <textarea
+          id="description"
+          type="text"
+          name="description"
+          placeholder="Description"
+          onChange={handleChange}
+        />
         <input id="myButton" type="submit" value="Calculate" onClick={toggleModal} />
       </form>
     </div>
