49acce9c4609f13d9d244252ae2ac94c479cd0dc
cristianUrbina <A01412249@itesm.mx>
2022-05-03 13:53:21 -0500

    Add sidebar to History page

--- src/Pages/History/index.jsx
+++ src/Pages/History/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import TopBar from "../../components/header/header.jsx";
+import Sidebar from "../../components/sidebar/sidebar.jsx";
 import HistoryTable from "../../components/historyTable/historyTable.jsx";
 import "./index.css";
 
@@ -28,6 +29,7 @@ export default function History() {
   return (
     <>
       <TopBar />
+      <Sidebar active="history"/>
       <div className="history">
         <h1>History</h1>
         <input type="text" id="search" name="search" placeholder="Search task by something" />
