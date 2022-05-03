cdd0609705b468c75c5aee8171ab67136e183300
cristianUrbina <A01412249@itesm.mx>
2022-05-03 02:58:24 -0500

    Style sidebar icons

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -1,12 +1,18 @@
-import React from "react";
-import MaterialIcon, {colorPalette} from 'material-icons-react';
+import React, { useState, useEffect } from "react";
+import MaterialIcon, { colorPalette } from "material-icons-react";
 import "./sidebar.css";
 
-export default function Sidebar() {
+export default function Sidebar(props) {
+  const { active } = props;
+
   return (
     <div className="sidebar">
-      <MaterialIcon icon="calculate" size="large"/>
-      <MaterialIcon icon="history" size="large"/>
+      <a href="/">
+        <MaterialIcon icon="calculate" size="large" color={ active == "calculate" ? "#003876" : ""}/>
+      </a>
+      <a href="/login">
+        <MaterialIcon icon="history" size="large" color={ active == "history" ? "#003876" : ""}/>
+      </a>
     </div>
   );
 }
