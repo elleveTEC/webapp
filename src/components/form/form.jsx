f8ccf0922774b9cd4791ec47673df5fc28666ebf
JoseLopez07 <a01283642@itesm.mx>
2022-06-03 16:14:52 -0500

    confirmar

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -18,6 +18,82 @@ const del_words = ['in', 'the', 'a','to', 'from', 'be', 'on', 'an', 'as', 'of', 
   console.log("Modelo cargado...");
 })();
 
+function processToken(text){
+  var token = [];
+  const array = text.match(/\w+/g);
+  for(var i = 0; i < del_words.length; i++){
+    while (array.includes(del_words[i])){
+      const idx = array.indexOf(del_words[i]);
+      array.splice(idx,1);
+    }
+  }
+  for(var i = 0; i < array.length; i++){
+    if (!dict.has(array[i])){
+      dict.set(array[i], dict.size + 1);
+    }
+    token[i] = dict.get(array[i]);
+  }
+  return token;
+}
+
+function top_words(tokens, words){
+  var valores = [];
+  var conteos = [];
+  for(var i = 0; i < tokens.length; i++){
+    if(valores.indexOf(tokens[i]) == -1){
+      valores.push(tokens[i]);
+      conteos.push(1);
+    } else {
+      conteos[valores.indexOf(tokens[i])]++;
+    }
+  }
+  var n_words = 0;
+  if(tokens.length > words){
+    var aux = [];
+    if (conteos.length >= words){
+      n_words = words;
+    }
+    else{
+      n_words = conteos.length;
+    }
+    for(var i = 0; i < n_words; i++){
+      const max_cont = Math.max(...conteos);
+      const max_index = conteos.indexOf(max_cont);
+      aux.push(valores[max_index]);
+      valores.splice(max_index, 1);
+      conteos.splice(max_index, 1);
+    }
+    tokens = aux;
+  }
+  if (tokens.length < words){
+    const dif = words - tokens.length
+    for(var i = 0; i < dif; i++){
+      tokens.push(0)
+    }
+    return tokens;
+  } else{
+    return tokens;
+  }
+}
+
+function getPrediction(){
+
+  const sum = document.getElementById("summary").value.toLowerCase();
+  const desc = document.getElementById("description").value.toLowerCase();
+  var sum_token = processToken(sum);
+  var desc_token = processToken(desc);
+  sum_token = top_words(sum_token, 5);
+  desc_token = top_words(desc_token, 20);
+  const predict_token = sum_token.concat(desc_token);
+
+  if (modelo != null){
+    var tensor = tf.tensor2d([predict_token]);
+    var prediccion = modelo.predict(tensor).dataSync();
+    prediccion = Math.round(prediccion);
+    console.log(prediccion);
+  }
+}
+
 export default function Form() {
     
   const [state, setState] = useState({
@@ -32,78 +108,7 @@ export default function Form() {
     const myButton = document.getElementById("myButton");
 
     myButton.addEventListener("click", (e) => {
-      function processToken(text){
-        var token = [];
-        const array = text.match(/\w+/g);
-        for(var i = 0; i < del_words.length; i++){
-          while (array.includes(del_words[i])){
-            const idx = array.indexOf(del_words[i]);
-            array.splice(idx,1);
-          }
-        }
-        for(var i = 0; i < array.length; i++){
-          if (!dict.has(array[i])){
-            dict.set(array[i], dict.size + 1);
-          }
-          token[i] = dict.get(array[i]);
-        }
-        return token;
-      }
-
-      function top_words(tokens, words){
-        var valores = [];
-        var conteos = [];
-        for(var i = 0; i < tokens.length; i++){
-          if(valores.indexOf(tokens[i]) == -1){
-            valores.push(tokens[i]);
-            conteos.push(1);
-          } else {
-            conteos[valores.indexOf(tokens[i])]++;
-          }
-        }
-        var n_words = 0;
-        if(tokens.length > words){
-          var aux = [];
-          if (conteos.length >= words){
-            n_words = words;
-          }
-          else{
-            n_words = conteos.length;
-          }
-          for(var i = 0; i < n_words; i++){
-            const max_cont = Math.max(...conteos);
-            const max_index = conteos.indexOf(max_cont);
-            aux.push(valores[max_index]);
-            valores.splice(max_index, 1);
-            conteos.splice(max_index, 1);
-          }
-          tokens = aux;
-        }
-        if (tokens.length < words){
-          const dif = words - tokens.length
-          for(var i = 0; i < dif; i++){
-            tokens.push(0)
-          }
-          return tokens;
-        } else{
-          return tokens;
-        }
-      }
-
-      const sum = document.getElementById("summary").value.toLowerCase();
-      const desc = document.getElementById("description").value.toLowerCase();
-      var sum_token = processToken(sum);
-      var desc_token = processToken(desc);
-      sum_token = top_words(sum_token, 5);
-      desc_token = top_words(desc_token, 20);
-      const predict_token = sum_token.concat(desc_token);
-
-      if (modelo != null){
-        var tensor = tf.tensor2d([predict_token]);
-        var prediccion = modelo.predict(tensor).dataSync();
-        console.log(prediccion);
-      }
-      
+      getPrediction();
       e.preventDefault();
     });
   }, []);
