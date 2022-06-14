35761032fc350a5e8ceb7b647ed92582d7386f5a
cristianUrbina <A01412249@itesm.mx>
2022-06-14 15:32:52 -0500

    Fix setState error inside Sidebar by removing Material icons

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom"
-import MaterialIcon from "material-icons-react";
+import { Link } from "react-router-dom";
+import HistoryIcon from "@mui/icons-material/History";
+import CalculateIcon from "@mui/icons-material/Calculate";
 import "./sidebar.css";
 
 export default function Sidebar() {
-
   const active = useSelector( state => state.pageIndicator.page );
 
   return (
     <div className="sidebar">
       <Link to="/">
-        <MaterialIcon icon="calculate" size="large" color={ active == "calculate" ? "#003876" : ""}/>
+        <CalculateIcon style={{ fontSize: 55, color: active == "calculate" ? "#003876": "gray", }} />
       </Link>
       <Link to="/history">
-        <MaterialIcon icon="history" size="large" color={ active == "history" ? "#003876" : ""}/>
+        <HistoryIcon style={{ fontSize: 55, color: active == "history" ? "#003876": "gray", }} />
       </Link>
     </div>
   );
